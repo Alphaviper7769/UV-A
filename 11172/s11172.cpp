361c40123db66e9f8815ff762d1f69121c4c34a5
Aayush Raturi <77016972+Alphaviper7769@users.noreply.github.com>
2024-05-05 15:28:37 +0530

    day1

---
+++ 11172/s11172.cpp
@@ -0,0 +1,21 @@
+/*
+11172 - Relational Operator
+*/
+#include <stdio.h>
+int main ()
+{
+    int t, i;
+    long a, b;
+    scanf("%d", &t);
+    for (i=1; i<=t; i++)
+    {
+        scanf("%ld %ld", &a, &b);
+        if (a < b)
+            printf("<\n");
+        if (a > b)
+            printf(">\n");
+        if (a==b)
+            printf("=\n");
+    }
+    return 0;
+}
---
+++ 1124/s1124.java
@@ -0,0 +1,15 @@
+/*
+Celebrity Jeopardy
+ */
+
+import java.util.*;
+class s1124
+{
+    public static void main(String[] args) 
+    {
+        Scanner x = new Scanner(System.in);
+        String s = x.nextLine();
+        System.out.println(s);
+        x.close();
+    }
+}
